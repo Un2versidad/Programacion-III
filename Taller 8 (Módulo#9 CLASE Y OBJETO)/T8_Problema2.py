8d95379bd815c563323e60e2a1709fc8b0424024
Fl2on <69091361+fl2on@users.noreply.github.com>
2025-03-13 19:32:53 -0500

    Add files via upload

--- Taller 8 (Módulo#9 CLASE Y OBJETO)/T8_Problema2.py
+++ Taller 8 (Módulo#9 CLASE Y OBJETO)/T8_Problema2.py
@@ -2,16 +2,16 @@
 class Empleados:
     def __init__(self):
         self.nombre = "Carlos"
-        self.sueldoBruto = 19.22
+        self.sueldoBruto = 900
 
     def mostrar(self) :
         print("Nombre: ", self.nombre)
-        print("Sueldo Bruto: ", f"{self.sueldoBruto:.2f}", "$")
+        print("Sueldo Bruto: ", f"${self.sueldoBruto:.2f}")
 
     def salario_neto(self):
         salario = self.sueldoBruto - (self.sueldoBruto * 0.17)
         salario = salario - (salario * 0.005)
-        print("Salario Neto: ", f"{salario:.2f}", "$")
+        print("Salario Neto: ", f"${salario:.2f}")
 
 empleados = Empleados()
 empleados.mostrar()
